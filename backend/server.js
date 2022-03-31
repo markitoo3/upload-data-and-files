524d8d7c2ce2c2124bf1b27b98bfee9ddd890e93
markitoo3 <dietrich.marko@gmail.com>
2022-03-31 09:47:33 +0200

    asd

---
+++ backend/server.js
@@ -0,0 +1,63 @@
+const express = require("express");
+const fileUpload = require("express-fileupload");
+const fs = require("fs");
+const path = require("path");
+const app = express();
+
+const dataLocation = path.join(`${__dirname}/../frontend/data/`);
+
+
+
+function getFunction(req, res){
+    res.sendFile(path.join(`${__dirname}/../frontend/index.html`));
+}
+
+app.use(fileUpload());
+app.use("/upload", express.static(`${__dirname}/../frontend/upload`));
+app.use("/pub", express.static(`${__dirname}/../frontend/public`));
+
+app.get("/", getFunction);
+
+// If there is a data.json, read the data from the file, if not, use an empty Array
+let jsonData = [];
+try {
+    let data = fs.readFileSync(`${dataLocation}data.json`, error => {
+        if (error) {
+            console.log(error);
+        }
+    });
+    jsonData = JSON.parse(data);
+} catch (error) {
+    console.log(error);
+}
+
+const uploads = path.join(`${__dirname}/../frontend/upload/`);
+
+app.post("/", (req, res) => {
+    // Upload image
+    const picture = req.files.picture;
+    const answer = {};
+    if (picture) {
+        console.dir(picture);
+        picture.mv(`${uploads}${picture.name}`);
+    }
+    answer.pictureName = picture.name
+    res.send(answer)
+
+    // Upload data from form
+    const formData = req.body;
+    formData.image_name = picture.name;
+    jsonData.push(formData);
+
+    fs.writeFile(`${dataLocation}data.json`, JSON.stringify(jsonData), (error) => {
+        if (error) {
+            console.log(error);
+        }
+    });
+});
+
+const port = 9000;
+const ipAddress = `http://127.0.0.1:${port}`;
+app.listen(port, () => {
+    console.log(ipAddress)
+});
---
+++ frontend/public/script.js
@@ -0,0 +1,42 @@
+const formComponent = `
+    <form id="form">
+        <input type="text" name="title">
+        <input type="file" name="picture">
+        <button>Send</button>
+    </form>
+`;
+
+function loadEvent() {
+    const rootElement = document.getElementById("root");
+
+    rootElement.insertAdjacentHTML("beforeend", formComponent);
+
+    const formElement = document.getElementById("form");
+    formElement.addEventListener("submit", e => {
+        e.preventDefault();
+
+        const formData = new FormData();
+        formData.append("title", e.target.querySelector(`input[name="title"]`).value);
+        formData.append("picture", e.target.querySelector(`input[name="picture"]`).files[0]);
+
+        const fetchSettings = {
+            method: "POST",
+            body: formData
+        };
+
+        fetch("/", fetchSettings)
+            .then(async data => {
+                if (data.status === 200) {
+                    const res = await data.json()
+                    e.target.outerHTML = `<img src="upload/${res.pictureName}">`;
+                    console.dir(data);
+                } 
+            })
+            .catch(error => {
+                e.target.outerHTML = `Error`;
+                console.dir(error);
+            })
+    });
+}
+
+window.addEventListener("load", loadEvent);
